9c6aa1fcd061bf082d7447adf0810ec269e9f4da
140103378+oxdev03@users.noreply.github.com <140103378+oxdev03@users.noreply.github.com>
2024-02-09 20:36:27 +0100

    use theme styling

--- dashboard/components/stats/ProcessItemMetric.tsx
+++ dashboard/components/stats/ProcessItemMetric.tsx
@@ -1,12 +1,10 @@
-import { Flex, Paper, Text, useMantineColorScheme } from '@mantine/core';
+import { Flex, Paper, Text } from '@mantine/core';
 import { TablerIconsProps } from '@tabler/icons-react';
+import classes from './ProcessItemMetric.module.css';
 
 export default function ProcessItemMetric({ w, Icon, value }: { w?: string; Icon: React.ElementType<TablerIconsProps>; value: string | undefined }) {
-  const { colorScheme } = useMantineColorScheme();
-
-  const dark = colorScheme === 'dark';
   return (
-    <Paper bg={dark ? 'dark.8' : 'gray.1'} radius="md" p={'4px'} px={'10px'}>
+    <Paper className={classes.processMetric} radius="md" p={'4px'} px={'10px'}>
       <Flex align={'center'} justify={'space-between'} gap={'5px'} w={w || '50px'}>
         <Icon size="1.2rem" />
         <Text size="md">{value || ''}</Text>
