f9938b94f80db37546189d5c6b3427bc838609cb
oxdev03 <140103378+oxdev03@users.noreply.github.com>
2024-03-23 13:47:28 +0100

    prettier include backend files

--- apps/dashboard/.eslintrc.js
+++ apps/dashboard/.eslintrc.js
@@ -2,6 +2,7 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
+  root: true,
   extends: ["next/core-web-vitals", "prettier"],
   ignorePatterns: ["node_modules", "dist"],
   parserOptions: {
