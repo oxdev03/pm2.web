77e503092c6cbb5d6b39f20d8185c8a9c1cb78a8
oxdev03 <140103378+oxdev03@users.noreply.github.com>
2024-04-14 21:14:13 +0200

    remove unused

--- apps/dashboard/components/hooks/useSSRPropsRefresh.tsx
+++ apps/dashboard/components/hooks/useSSRPropsRefresh.tsx
@@ -12,6 +12,7 @@ const useRefreshSSRProps = (refetchInterval: number) => {
     );
 
     return () => clearInterval(interval);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
